49a116bcb7aadd5aa127ee2818ca8d2d40726693
Jack Gowan <jackgowan@gmail.com>
2021-02-03 12:52:21 +0000

    My Django Girls app, debug 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,8 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 #stuff I added to try and fix the bug!!!
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # BASE_DIR = os.path.dirname(os.path.dirname(__file__))
