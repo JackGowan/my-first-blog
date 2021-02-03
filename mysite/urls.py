1f2915a3fab1217db5c6bf0fa2b7e76dd39cfe24
Jack Gowan <jackgowan@gmail.com>
2021-02-03 15:37:31 +0000

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #this is setting it to the home page I understand:
+    path('', include('blog.urls')),
 ]
+
+
